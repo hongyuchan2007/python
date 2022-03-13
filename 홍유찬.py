5f17eb4d549a4bb9139e35d28cc76683769a6c74
hongyuchan2007 <101329322+hongyuchan2007@users.noreply.github.com>
2022-03-13 16:50:20 +0900

    Add files via upload

---
+++ 홍유찬.py
@@ -0,0 +1,63 @@
+# name1 = "김민수"
+# age1 = 10
+# name2 = "이철희"
+# age2 = 13
+# print("이름: %s 나이: %d" %(name1, age1))
+# print("이름: %s 나이: %d" %(name2, age2))
+# print("이름: {0} 나이 : {1}".format(name1, age1))
+#print("이름: {0} 나이 : {1}".format(name2, age2))
+# print(f"이름: {name1} 나이 : {age1}")
+# print(f"이름: {name2} 나이 : {age2}")
+# 상장주식수 = "5,969,782,550"
+# print(상장주식수.replace(",",""))
+# print(int(상장주식수.replace(",","")))
+# 분기 = "2020/03(e) (IFRS연결)"
+# print(분기[:7])
+# data = "   삼성전자    "
+# print("" + data.strip(" ") + "ㅇ")
+# ticker = 'btc_krw'
+# print(ticker.upper())
+# a = "hello"
+# a= a.capitalize()
+# print(a)
+# a = "hello-world"
+# print(a.split("-"))
+a = [4,2,1,3]
+# # a.append(5) #append 함수는 리스트의 마지막 인덱스의 값 추가
+#print(a)
+#a.sort(reverse= True)
+#print(a)
+#print(a.index(4))
+# a.insert(3,5)
+# print(a)
+# a.remove(1)
+# print(a)
+# b = [6,7]
+# a.append(b) a.expend(b)
+# print(a)
+# movie_rank = ["닥터스트레인지","스플릿","럭키"]
+# movie_rank.append("배트맨")
+# print(movie_rank)
+# movie_rank.insert(1,"슈퍼맨")
+# movie_rank.remove("럭키")
+# print(movie_rank)
+# lang1 = ["c", "c++","java"]
+# lang2 = ["python", "go","c#"]
+# langs = lang1+lang2
+# print(langs)
+# nums = [1,2,3,4,5]
+# a = sum(nums)
+# b = len(nums)
+# print(a/b)
+# price = ['a',10,20,30]
+# print(price[1:])
+# nums = [1,2,3,4,5,6,7,8,9,10]
+# print(nums[1::1])
+#interest = ['삼성전자','LG전자','Naver','SK하이닉스','미래에셋대우']
+#interest.remove('LG전자')
+#print(interest)
+#print("\n".join(interest))
+#string = "삼성전자/LG전자/Naver"
+data = [2,4,3,1,5,10,9]
+data.sort()
+print(data)
