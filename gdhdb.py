622db79083376eb40768d460a0509deb695dc23e
hongyuchan2007 <101329322+hongyuchan2007@users.noreply.github.com>
2022-03-21 19:47:49 +0900

    Add files via upload

--- gdhdb.py
+++ gdhdb.py
@@ -1,71 +1,57 @@
-#items = (key,value)의 형태로 나온다.z
-# result = zip(keys,vals)
-# # print(dict(result))
-# s1 =  set([1])
-# s2 = set([1,2])
-# s3 = set([3,4])
-# print(s2 | s3)
-# print(s2 - s3)
-# print(s2 & s3)
-# s1.remove(1)
-# s1.add(4)
-# s1.update([2,4,5])
-# print(s1)
-# a = [1,2,3,3,4,5,4,3,2,1]
-# b = set(a)
-# print
-# print(bool('python'),bool(""))
-# print(bool([1,2,3]),bool([]))
-# print(bool((1,2)),bool(()))
-# print(bool({'1':1}),bool({}))
-# print(bool(-2),bool(0))
-# print(bool(None)) bool = 불 또는 불린 타입
-# a = input()
+# a = (input('입력'))
+# d =  {'달러':1167,'엔':1.096,'유로':1268,'위안':171}
+# # f = a.split()
+# # won = d[f[1]] *  int(f[0])
+# # print("%.2f"%won,'원')
+# a,b= input().split()
 # print(a)
-# B = int(input())
-# # print(B)
-# # a = input()
-# # print(a*2)
-# if 조건문:
-#     실행문(조건이 참일 떄)
-# else:(위의 조건문을 재외한 나머지 경우)
-#     실행문
+# print(b)
 # a = int(input())
-# if a % 2 ==0:
-#     print('짝수')
+# b = int(input())
+# c = int(input())
+# if a>b and a > c:
+#     print(a)
+# elif b>a and b > c:
+#     print(b)
 # else:
-#     print('홀수')
-# a = int(input())
-# if a+20>255:
-#     print(255)
+#     print(c)
+# d = {'011':'skt','016':'kt','019':'lgu','010':'알수없음'}
+# a = input('휴대전화 번호 입력')
+# print('당신은 %s 사용자입니다'%d[a[:3]]
+# a = input('우편번호:')
+# if a[:3] in ['010','011','012']:
+#     print('강북구')
+# elif a[:3] in ['013','014','015']:
+#     print('도봉구')
+# elif a[:3] in ['016','017','018','019']:
+#     print('노원구')
+# a = input('주민등록번호:')
+# if a[7]== '1' or a[7] == '3':
+#     print('남자')
+# elif a[7]== '2' or a[7] == '4':
+#     print('여자')
+# a = input('주민등록번호:')
+# if a[8:10] in ['01','02','03','04','05','06','07','08']:
+#     print('서울입니다')
 # else:
-#     print(a+20)\
-# a = int(input())
-# if a-20>255:
-#     print(255)
-# elif a-20<0:
-#     print(0)
+#     print('서울이 아닙니다')
+# 주민번호 = input("주민등록번호: ")
+# 뒷자리 = 주민번호.split("-")[1]
+# if 0 <= int(뒷자리[1:3]) <= 8:
+#     print("서울입니다.")
 # else:
-#     print(a-20)
-# a= input("현재시간:")
-# if a[3:] =='00':
-#     print('정각입니다')
-# else:
-#     print('정각이 아닙니다')
-# fruit = ["사과", "포도", "홍시"]
+#     print("서울이 아닙니다.")
 # a = input()
-# if a in fruit:
-#     print('정답입니다')
+# b =int(a[0])*2 + int(a[1])*3 + int(a[2])*4 + int(a[3])*5 + int(a[4])*6 + int(a[5])*7 + int(a[7])*8 + int(a[8])*9 + int(a[9])*2 + int(a[10])*3 + int(a[11])*4 + int(a[12])*5
+#
+# print(b)
+# c = b%11
+# print(c)
+# print(11-c)
+# if 11-c == int(a[-1]):
+#     print('유효')
 # else:
-#     print('오답입니다')
-# a = int(input('score:'))
-# if 81<=a<=100:
-#     print("A")
-# elif 61 <= a <= 80:
-#     print("B")
-# elif 41 <= a <= 60:
-#     print("C")
-# elif 21 <= a <= 40:
-#     print("D")
-# elif 0 <= a <= 20:
-#     print("E")
+#     print('유효x')
+# sum= 1
+# sum+=2
+# print(sum)
