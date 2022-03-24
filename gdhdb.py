199f1c8fe1cc26f6e9090d2770374c6d28dd1fa6
hongyuchan2007 <101329322+hongyuchan2007@users.noreply.github.com>
2022-03-24 19:48:12 +0900

    Update gdhdb.py

--- gdhdb.py
+++ gdhdb.py
@@ -55,3 +55,96 @@
 # sum= 1
 # sum+=2
 # print(sum)
+# 리스트 = ["가", "나", "다", "라"]
+# # for i in range(1,4):
+# #     print(리스트[i])
+# # for i in 리스트[::-1]:
+# #     print(i)
+# 리스트 = [3, -20, -3, 44]
+# for i in 리스트:
+#     if i<0:
+#         print(i)
+# 리스트 = [3, 100, 23, 44]
+# for i in 리스트:
+#     if i%3 == 0:
+#         print(i)
+# 리스트 = [13, 21, 12, 14, 30, 18]
+# for i in 리스트:
+    # if i<20:
+    #     if i%3 == 0:
+    #         print(i)
+    # if i<20 and i%3 == 0:
+    #     print(i)
+# 리스트 = ["I", "study", "python", "language", "!"]
+# for i in 리스트:
+#     if len(i) >=3:
+#         print(i)
+# 리스트 = ["A", "b", "c", "D"]
+# for i in 리스트:
+#     if i.isupper():
+#         print(i)
+# 리스트 = ['hello.py', 'ex01.py', 'intro.hwp']
+# for i in 리스트:
+#     print(i.split('.')[0])
+# 리스트 = ['intra.h', 'intra.c', 'define.h', 'run.py']
+# for i in 리스트:
+#     if i.split('.')[1] =='h' or i.split('.')[1] =='c':
+#         print(i)
+# for i in range(1,10):
+#     print(i/10)
+# for i in range(1,10):
+#     if i%2 != 0:
+#         print('3 x %d=%d' % (i, 3 * i))
+# a = 1
+# for i in range(1,11,):
+#     a *= i
+#     print(a)
+# price_list = [32100, 32150, 32000, 32500]
+# for i in range(len(price_list)):
+#     print(3-i,price_list[i])
+# price_list = [32100, 32150, 32000, 32500]
+# for i in range(1,len(price_list)):
+# #     print(90+i*10,price_list[i])
+# my_list = ["가", "나", "다", "라", "마"]
+# for i in range(len(my_list)-2):        # i 0 = 0~2
+#     print(my_list[i],my_list[i+1],my_list[i+2])
+# my_list = ["가", "나", "다", "라"]
+# for i in range(1,len(my_list)):
+#     print(my_list[-i],my_list[-i-1])
+# my_list = [100, 200, 400, 800]
+# for i in range(len(my_list)-1):
+#     print(my_list[i+1]-my_list[i])
+# my_list = [100, 200, 400, 800, 1000, 1300]
+# for i in range(len(my_list)-2):         #i=0 ~ 3
+#      print((my_list[i]+my_list[i+1]+my_list[i+2])/3)
+# low_prices  = [100, 200, 400, 800, 1000]
+# high_prices = [150, 300, 430, 880, 1000]
+# volatility = []
+# for i in range(len(high_prices)):
+#     volatility.append(high_prices[i] - low_prices[i])
+# print(volatility)
+# apart = [['101호','102호'],['201호','202호'],['301호','302호']]
+# stock = [['시가',100,200,300],['종가',80,210,330]]
+# stock = {stock[0][0]:stock[0][1],stock[1][0]:stock[1][1]}
+# apart = [ [101, 102], [201, 202], [301, 302] ]
+# for i in range(len(apart)):
+# #     for x in range(len(apart)[0])):
+#         print(apart[i][x])
+# apart = [ [101, 102], [201, 202], [301, 302] ]
+# for i in range(1,len(apart)+1):
+#     for j in range(len(apart[0])):
+#         print(apart[-i][j])
+# apart = [ [101, 102], [201, 202], [301, 302] ]
+# for i in range(1,len(apart)+1):
+#     for j in range(len(apart[0])):
+#         print(apart[-i][j])
+# apart = [ [101, 102], [201, 202], [301, 302] ]
+# for i in range(1,len(apart)+1):
+#     for j in range(len(apart[0])):
+#         print(apart[-i][-j])
+# apart = [ [101, 102], [201, 202], [301, 302] ]
+# for i in range(len(apart)):
+#     for j in range(len(apart[0])):
+#         print(apart[i][j])
+#     print('-----')
+    
