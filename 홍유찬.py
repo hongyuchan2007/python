743b0fc7b5a735d92b26c0c86ee1c8e81422daa0
hongyuchan2007 <101329322+hongyuchan2007@users.noreply.github.com>
2022-03-28 19:50:04 +0900

    Add files via upload

--- 홍유찬.py
+++ 홍유찬.py
@@ -1,63 +1,112 @@
-# name1 = "김민수"
-# age1 = 10
-# name2 = "이철희"
-# age2 = 13
-# print("이름: %s 나이: %d" %(name1, age1))
-# print("이름: %s 나이: %d" %(name2, age2))
-# print("이름: {0} 나이 : {1}".format(name1, age1))
-#print("이름: {0} 나이 : {1}".format(name2, age2))
-# print(f"이름: {name1} 나이 : {age1}")
-# print(f"이름: {name2} 나이 : {age2}")
-# 상장주식수 = "5,969,782,550"
-# print(상장주식수.replace(",",""))
-# print(int(상장주식수.replace(",","")))
-# 분기 = "2020/03(e) (IFRS연결)"
-# print(분기[:7])
-# data = "   삼성전자    "
-# print("" + data.strip(" ") + "ㅇ")
-# ticker = 'btc_krw'
-# print(ticker.upper())
-# a = "hello"
-# a= a.capitalize()
+# data = [
+#     [ 2000,  3050,  2050,  1980],
+#     [ 7500,  2050,  2050,  1980],
+#     [15450, 15050, 15550, 14900]
+# ]
+# for i in range(len(data)):
+#     for x in range(len(data)+1):
+#         print(data[i][x]+data[i][x]*0.00014)
+#     print('----------')
+# data = [
+#     [ 2000,  3050,  2050,  1980],
+#     [ 7500,  2050,  2050,  1980],
+#     [15450, 15050, 15550, 14900]
+# ]
+# result = []
+# for i in range(len(data)):
+#     for x in range(len(data)+1):
+#         result.append(data[i][x]+data[i][x]*0.00014)
+# print(result)
+# result = []
+# for i in range(len(data)):
+#     a = []
+#     for x in range(len(data)+1):
+#         a.append(data[i][x]+data[i][x]*0.00014)
+#     result.append(a)
+# print(result)
+# ohlc = [["open", "high", "low", "close"],
+#         [100, 110, 70, 100],
+#         [200, 210, 180, 190],
+#         [300, 310, 300, 310]]
+# for i in range(len(ohlc)-1):
+#     print(ohlc[i+1][3])
+# ohlc = [["open", "high", "low", "close"],
+#         [100, 110, 70, 100],
+#         [200, 210, 180, 190],
+#         [300, 310, 300, 310]]
+# for i in range(len(ohlc)-1):
+#     if ohlc[i+1][3] > 150:
+#         print(ohlc[i+1][3])
+# ohlc = [["open", "high", "low", "close"],
+#         [100, 110, 70, 100],
+#         [200, 210, 180, 190],
+#         [300, 310, 300, 310]]
+# for i in range(len(ohlc)-1):
+#     if ohlc[i + 1][3] >= ohlc[i + 1][0] :
+#         print(ohlc[i + 1][3])
+# ohlc = [["open", "high", "low", "close"],
+#         [100, 110, 70, 100],
+#         [200, 210, 180, 190],
+#         [300, 310, 300, 310]]
+# volatility = []
+# for i in range(len(ohlc)-1):
+#     volatility.append(ohlc[i+1][1]-ohlc[i+1][2])
+# print(volatility)
+# ohlc = [["open", "high", "low", "close"],
+#         [100, 110, 70, 100],
+#         [200, 210, 180, 190],
+#         [300, 310, 300, 310]]
+# for i in range(1,len(ohlc)):
+#     if ohlc[i][0] < ohlc[i][3]:
+#         print(ohlc[i][1]-ohlc[i][2])
+# ohlc = [["open", "high", "low", "close"],
+#         [100, 110, 70, 100],
+#         [200, 210, 180, 190],
+#         [300, 310, 300, 310]]
+# a = 0
+# for i in range(1,len(ohlc)):
+#     a +=ohlc[i][3]-ohlc[i][0]
 # print(a)
-# a = "hello-world"
-# print(a.split("-"))
-a = [4,2,1,3]
-# # a.append(5) #append 함수는 리스트의 마지막 인덱스의 값 추가
-#print(a)
-#a.sort(reverse= True)
-#print(a)
-#print(a.index(4))
-# a.insert(3,5)
-# print(a)
-# a.remove(1)
-# print(a)
-# b = [6,7]
-# a.append(b) a.expend(b)
-# print(a)
-# movie_rank = ["닥터스트레인지","스플릿","럭키"]
-# movie_rank.append("배트맨")
-# print(movie_rank)
-# movie_rank.insert(1,"슈퍼맨")
-# movie_rank.remove("럭키")
-# print(movie_rank)
-# lang1 = ["c", "c++","java"]
-# lang2 = ["python", "go","c#"]
-# langs = lang1+lang2
-# print(langs)
-# nums = [1,2,3,4,5]
-# a = sum(nums)
-# b = len(nums)
-# print(a/b)
-# price = ['a',10,20,30]
-# print(price[1:])
-# nums = [1,2,3,4,5,6,7,8,9,10]
-# print(nums[1::1])
-#interest = ['삼성전자','LG전자','Naver','SK하이닉스','미래에셋대우']
-#interest.remove('LG전자')
-#print(interest)
-#print("\n".join(interest))
-#string = "삼성전자/LG전자/Naver"
-data = [2,4,3,1,5,10,9]
-data.sort()
-print(data)
+#except= 예외
+# while True:
+#     a,b = input().split(',')
+#     print(int(a)+ int(b))
+#     if a == '0' and b == '0':
+#         break
+# T = int(input())
+# for i in range(T):
+#     a,b = input().split()
+#     print("case #%d :%d + %d = %d"%(i+1, int(a), int(b),int(a)+int(b)))
+# a, b = input().split( )
+# if int(a) > int(b):
+#     print('>')
+# elif int(a) < int(b):
+#     print('<')
+# else:
+#     print("==")
+# a = int(input())
+# if a % 4 == 0 and a % 100 !=0 or a % 400 == 0:
+#     print(1)
+# else:
+#     print(0)
+# x = int(input())
+# y = int(input())
+# if  x > 0 and y>0:
+#     print(1)
+# elif x < 0 and y>0:
+#     print(2)
+# elif x < 0 and y<0:
+#     print(3)
+# else:
+#     print(4)
+H, M = input().split()
+H = int(H)
+M = int(M)
+if M < 45:
+    if H == 0:
+        H += 23
+        M += 60
+    else:
+        H -= 1
+        M += 60
+print(H,M-45)
