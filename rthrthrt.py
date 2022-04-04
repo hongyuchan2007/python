76105b7a444d8a8a5b76c17a49bdda7d799b3dc7
hongyuchan2007 <101329322+hongyuchan2007@users.noreply.github.com>
2022-04-04 20:30:51 +0900

    Add files via upload

---
+++ rthrthrt.py
@@ -0,0 +1,41 @@
+# a = int(input())
+#
+# for i in range(a):
+#     #Todo 점수를 받아줄 변수 필요 O가 나왔을 때 받아줄 변수 필요
+#     정답지 = input()
+#     b = 0   #최종점수
+#     c = 0    #o가 나왔을 때 점수
+#     for j in 정답지:
+#         if j =='O':
+#             c += 1
+#             b += c
+#         else:
+#             c = 0
+#     print(b)
+# a = int(input())
+# for i in range(a):
+#     b = input().split()
+#     c = []
+#     for j in b:
+#         c.append(int(j))
+#     avg = 0  #sum은 학생들의 총 점수
+#     for k in range(1,len(c)):
+#         avg += c[k]/c[0]
+#     cnt = 0
+#     for l in range(1,len(c)):
+#         if c[l] > avg:
+#             cnt += 1
+#     rate = cnt/c[0] * 100
+#     print(f"{rate:.3f}%")
+#ord() = 아스키 코드로 변환
+# a = input()
+# c = input()
+# b=0
+# for i in c:
+#     b += int(i)
+# print(b)
+a = input()
+#chr 은 아스키 코드로 나타내는 걳
+# x = input()
+# for i in range(97,123):
+#     print(x.find(chr(i)),end=' ')
