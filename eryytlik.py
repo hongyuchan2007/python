ee408e0ee8d4a23075b671f90d46b5773cef3355
hongyuchan2007 <101329322+hongyuchan2007@users.noreply.github.com>
2022-04-11 19:46:20 +0900

    Add files via upload

---
+++ eryytlik.py
@@ -0,0 +1,31 @@
+# a = int(input())
+# cnt = a
+# g = []
+# for i in range(a):
+#     str= input()
+#     for j in range(len(str)-1):
+#         if str[j] == str[j+1]:
+#             pass
+#         elif str[j] in str[j+1:]:
+#             cnt -= 1
+#             break
+# print(cnt)
+# str = input()
+# word = len(str)
+# for i in range(len(str)):
+#     if str [i] == '=':
+#         if str[i-1] in ['c','s']:
+#             word -=1
+#         elif str[i-1] =='z':
+#             if str[i-2] == 'd':
+#                 word -=2
+#             else:
+#                 word -= 1
+#     elif str[i] == '-':
+#         if str [i-1] in ['c','d']:
+#             word -=1
+#     elif str[i] == 'j':
+#         if str[i-1] in ['l','n']:
+#             word -= 1
+# print(word)
+def si
