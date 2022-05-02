db946e0a58434941375ef6c31be907b8a0b7d9dc
hongyuchan2007 <101329322+hongyuchan2007@users.noreply.github.com>
2022-05-02 16:10:02 +0900

    Add files via upload

---
+++ uhjvgc.py
@@ -0,0 +1,16 @@
+def solution(new_id):
+    answer = ''
+    new_id.lower()
+    if new_id not in 'qazwsxedcrfvtgbyhnujmikolp1234567890-_.':
+
+    if new_id
+
+    if new_id[0:] == '.' or new_id[-1:] == '.'
+
+    if len(new_id) > 15:
+
+    if new_id[-1:] == '.':
+
+    if len(new_id) < 3:
+
+    return answer
