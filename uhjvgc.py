7df92e1da69ed1ac3c639a29f5055492f1e1df9b
hongyuchan2007 <101329322+hongyuchan2007@users.noreply.github.com>
2022-05-09 16:28:44 +0900

    Add files via upload

--- uhjvgc.py
+++ uhjvgc.py
@@ -1,16 +1,58 @@
-def solution(new_id):
-    answer = ''
-    new_id.lower()
-    if new_id not in 'qazwsxedcrfvtgbyhnujmikolp1234567890-_.':
+class dequeelement:
+    def __init__(self,data,left,right):
+        self.data = data
+        self.left = left
+        self.right = right
+class deque:
+    def __init__(self):
+        self.rear = None
+        self.front = None
+    def insert_rear(self,data):
+        if self.rear is None:
+            elem = dequeelement(data,None,None)
+            self.rear = elem
+            self.front = elem
+        else:
+            elem = dequeelement(data, None, self.rear)
+            self.rear.left = elem
+            self.rear = elem
+    def insert_front(self,data):
+        if self.front is None:
+            elem = dequeelement(data,None,None)
+            self.front = elem
+            self.rear = elem
+        else:
+            elem = dequeelement(data,self.front,None)
+            self.front.right = elem
+            self.front = elem
+    def delete_rear(self):
+        if self.rear is None:
+            return None
+        temp = self.rear
+        if self.rear == self.front:
+            self.front=self.rear = None
+            return temp.data
+        else:
+            temp = self.rear
+            self.rear = self.rear.right
+            self.rear.left = None
+            return temp.data
+    def delete_front(self):
+        if self.front is None:
+            return None
+        temp = self.front
+        if self.rear == self.front:
+            self.rear = self.front = None
+            return temp.data
+        else:
+            self.front = self.front.left
+            self.front.right = None
+            return temp.data
+    #TODO reverse 구현
+    def reverse(self):
+        a = None
+        b = self.rear
+        c = self.front
 
-    if new_id
-
-    if new_id[0:] == '.' or new_id[-1:] == '.'
-
-    if len(new_id) > 15:
-
-    if new_id[-1:] == '.':
-
-    if len(new_id) < 3:
-
-    return answer
+        while b:
+            self
